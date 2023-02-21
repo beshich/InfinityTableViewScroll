ed4a26defe0600b90b93f81eeceadeb729fc21b1
Agatay Embeev <106145589+beshich@users.noreply.github.com>
2023-02-21 22:31:33 +0600

    feat: создание протокола TableViewDelegate/DataSource

---
+++ InfinityTableViewScroll/InfinityTableViewScroll/CoreLayer/Protocols/TableViewProtocols.swift
@@ -0,0 +1,10 @@
+//
+//  TableViewProtocols.swift
+//  InfinityTableViewScroll
+//
+//  Created by Agatay Embeev on 21.02.2023.
+//
+
+import UIKit
+
+public typealias TableViewDelegateAndDataSourcesProtocol = UITableViewDelegate & UITableViewDataSource
