be24265dc2a885b42ad0f9fc370b0a37662d2bbb
Agatay Embeev <106145589+beshich@users.noreply.github.com>
2023-02-22 23:29:26 +0600

    feat: добавление для всего проекта колбэков

---
+++ InfinityTableViewScroll/InfinityTableViewScroll/CoreLayer/Callbacks/Callbacks.swift
@@ -0,0 +1,11 @@
+//
+//  Callbacks.swift
+//  InfinityTableViewScroll
+//
+//  Created by Agatay Embeev on 22.02.2023.
+//
+
+import UIKit
+
+typealias VoidCallback = () -> Void
+typealias Callback<T> = (T) -> Void
