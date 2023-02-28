cb9055b59989f8ab91eeeb9d1966e916f64b9881
Agatay Embeev <106145589+beshich@users.noreply.github.com>
2023-02-28 13:25:14 +0600

    feat: добавление общего контроллера

---
+++ InfinityTableViewScroll/InfinityTableViewScroll/CoreLayer/Utilis/TableViewController/TableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewController.swift
+//  InfinityTableViewScroll
+//
+//  Created by Agatay Embeev on 28.02.2023.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
