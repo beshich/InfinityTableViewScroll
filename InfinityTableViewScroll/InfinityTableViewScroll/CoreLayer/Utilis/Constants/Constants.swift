453600da70e05e6e6998e17e24d737041c721d31
Agatay Embeev <106145589+beshich@users.noreply.github.com>
2023-03-04 18:54:05 +0600

    feat: создание Constants для использования

---
+++ InfinityTableViewScroll/InfinityTableViewScroll/CoreLayer/Utilis/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  InfinityTableViewScroll
+//
+//  Created by Agatay Embeev on 04.03.2023.
+//
+
+import Foundation
+
+struct Constants {
+    
+    // MARK: Properties:
+    
+    static let baseUrl = ApiClient.shared.baseUrl
+}
