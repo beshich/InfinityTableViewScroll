e69fa4a9349e0ca4ccb45787b5fc9b68d150dea2
Agatay Embeev <106145589+beshich@users.noreply.github.com>
2023-03-06 13:40:12 +0600

    feat: добавление DetailsViewController/ViewModel

---
+++ InfinityTableViewScroll/InfinityTableViewScroll/Flows/Details/ViewController/DetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.swift
+//  InfinityTableViewScroll
+//
+//  Created by Agatay Embeev on 06.03.2023.
+//
+
+import UIKit
+import WebKit
+
+final class DetailsViewController: UIViewController, WKUIDelegate {
+    
+}
---
+++ InfinityTableViewScroll/InfinityTableViewScroll/Flows/Details/ViewModel/DetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsViewModel.swift
+//  InfinityTableViewScroll
+//
+//  Created by Agatay Embeev on 06.03.2023.
+//
+
+import Foundation
