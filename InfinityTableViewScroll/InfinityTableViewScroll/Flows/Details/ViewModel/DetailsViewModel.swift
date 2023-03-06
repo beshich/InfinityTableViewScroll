10d39ebf772f6192da30c1b8f7b787e031a46de3
Agatay Embeev <106145589+beshich@users.noreply.github.com>
2023-03-06 14:29:14 +0600

    feat: добавление параметров во viewModel

--- InfinityTableViewScroll/InfinityTableViewScroll/Flows/Details/ViewModel/DetailsViewModel.swift
+++ InfinityTableViewScroll/InfinityTableViewScroll/Flows/Details/ViewModel/DetailsViewModel.swift
@@ -5,4 +5,17 @@
 //  Created by Agatay Embeev on 06.03.2023.
 //
 
-import Foundation
+import UIKit
+
+final class DetailsViewModel {
+    
+    // MARK: = Properties:
+    
+    var url: String?
+    
+    // MARK: Lifecycle:
+    
+    init(url: String? = nil) {
+        self.url = url
+    }
+}
